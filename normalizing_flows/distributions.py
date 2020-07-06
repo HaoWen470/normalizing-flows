Improve optimization numerical stability and add minor fixes

normalizing_flows/distributions.py
@@ -15,7 +15,7 @@ class DistributionParameters:
     def log_likelihood(self, inputs) -> tf.Tensor: raise NotImplementedError()
     def sample_batch(self, batch_size: int = 1) -> tf.Tensor: raise NotImplementedError()
     def sample(self) -> tf.Tensor: 
-        return tf.squeeze(self.sample_batch(batch_size=self.parameters_batch_rank), 1)
+        return tf.squeeze(self.sample_batch(batch_size=1), self.parameters_batch_rank)
 
 
 class ParameterizableDistribution(tf.Module):

normalizing_flows/flows.py
@@ -8,7 +8,7 @@ class FlowParameters:
     def transform(self, inputs): raise NotImplementedError()
     def jacobian_determinant(self, inputs): raise NotImplementedError()
     def jacobian_log_determinant(self, inputs):
-        return tf.math.log(self.jacobian_determinant(inputs))
+        return tf.math.log(self.jacobian_determinant(inputs) + 1e-8)
 
 
 class Flow(tf.Module):
@@ -73,6 +73,6 @@ class PlanarFlow(Flow):
 
         w_dot_u = tf.reduce_sum(w * u, axis=-1, keepdims=True)
         w_squared_norm = tf.reduce_sum(tf.square(w), axis=-1, keepdims=True) + 1e-8
-        u_hat = u + (-1 + tf.nn.softplus(w_dot_u) - w_dot_u) * w / w_squared_norm
+        u_hat = u + (-1. + tf.nn.softplus(w_dot_u) - w_dot_u) * w / w_squared_norm
 
         return PlanarFlowParameters(w=w, u=u_hat, b=b)

normalizing_flows/models.py
@@ -19,12 +19,12 @@ class NormalizingFlowImageModel(tf.Module):
             [
                 PlanarFlow(400) for _ in range(num_flows)
             ])
-        
+
         self.generative_model = DLGM([
             StandardGaussianLayer(400),
             IndependentBernoulliLayer(self.image_size)
         ])
-    
+
     def preprocess_images(self, image):
         image_shape = tf.shape(image)
         output_shape = tf.concat([image_shape[:-3], [self.image_size]], 0)

normalizing_flows/trainer.py
@@ -75,7 +75,7 @@ class NormalizingFlowsTrainer:
                     generator_ll = generator_params.log_likelihood([recognition_samples, observation_samples])
 
                     # Formulate the loss.
-                    loss = -tf.reduce_mean(approx_posterior_ll + beta * generator_ll / tf.cast(sample_size, tf.float32))
+                    loss = -tf.reduce_mean(approx_posterior_ll + beta * generator_ll)
 
                 tf.debugging.check_numerics(loss, 'loss has invalid numerics')
 
@@ -87,8 +87,9 @@ class NormalizingFlowsTrainer:
                 optimizer.apply_gradients(zip(gradients, trainable_variables))
 
                 # Write summaries.
-                with summary_writer.as_default():
-                    tf.summary.scalar('loss', loss, step=step)
+                if step % summary_decimation == 0:
+                    with summary_writer.as_default():
+                        tf.summary.scalar('loss', loss, step=step)
 
                 # Print info.
                 print('Step =', step.numpy(), ', Loss =', loss.numpy())